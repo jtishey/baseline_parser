8ee62130c237adefc075b4bbd7a383edcb873d5f
John <johntishey@gmail.com>
2017-09-16 18:35:47 -0400

    update

--- modules/the_differentiator.py
+++ modules/the_differentiator.py
@@ -4,19 +4,19 @@ baseline_compare module to compare command output from Pinky_baseliner
 john.tishey@windstream.com 2017
 """
 
-import yaml
+from modules import yaml
 
 # Step 1: Loop through commands, using pre-determined logic where available
 def run(device):
     """ Compare output of commands according to test rules
         device['output']['before'] and device['output']['after'] contain the outputs """
     project_path = os.path.abspath('/opt/ipeng/scripts/baseline_parser/')
-    with open(project_path + 'config.yml') as _f:
+    with open(project_path + '/config.yml') as _f:
         config = yaml.load(_f)
-    test_list = config[device['os-type']]
-    test_path = project_path + 'testfiles/' + device['os_type'] + '/'
+    test_list = config[(device['os_type'])]
+    test_path = project_path + '/testfiles/' + device['os_type']
     for test_case in test_list:
-        with open(test_path + test_case) as _f:
+        with open(test_path + '/' + test_case) as _f:
             test_values = yaml.load(_f.read())
         before_cmd = device['output']['before'][test_values[0]['command']]
         after_cmd = device['output']['after'][test_values[0]['command']]
@@ -39,18 +39,18 @@ def execute_diff(test_values, before, after):
         if skip_line is not True:
             if 'no-diff' in test_values[0]['tests'][0]:
                 line = line.split()
-                line_id = line[test_values[0]]['tests'][0]['index'][0]
+                line_id = test_values[0]['tests'][0]['no-diff'][0]
                 for i, after_line in enumerate(after):
                     after_line = after_line.split()
                     if line[line_id] == after_line[line_id]:
-                        for index in test_values[0]]['tests'][0]['index'][0]:
+                        for index in test_values[0]['tests'][0]['no-diff']:
                             if line[index] != after_line[index]:
                                 print("FAIL: " + line[line_id] + " was " + line[index] +" and is now " + after_line[index])
                                 fail_flag = True
                         if fail_flag == False:
                             after.pop(i)
             if fail_flag == True:
-                print(test_values[0]['tests'][0]['err'])
+                print(test_values[0]['tests'][0]['err']) 
             else:
-                print(test_values[0]['tests'[0]['info'])
-                    
+                print(test_values[0]['tests'][0]['info'])
+                print line[0] + ' - ' + line[1] + ' - ' + line[3]
