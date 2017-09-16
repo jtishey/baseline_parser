13da61e73c87fa649d2d0bb55596de80215068d8
john <johntishey@gmail.com>
2017-09-16 10:56:38 -0400

    new files

--- modules/the_differentiator.py
+++ modules/the_differentiator.py
@@ -5,4 +5,8 @@ john.tishey@windstream.com 2017
 """
 
 # Step 1: Loop through commands, using pre-determined logic where available
-
+def run(device):
+    """ Compare output of commands according to test rules
+        device['output']['before'] and device['output']['after'] contain the outputs """
+    if device['os_type'] == 'JUNOS':
+        pass
