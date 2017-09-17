9546a83b66c473860dea36e55d411cd721bc2b7c
John <johntishey@gmail.com>
2017-09-16 23:41:36 -0400

    updates

--- baseline_parser.py
+++ baseline_parser.py
@@ -8,50 +8,49 @@ John.Tishey@windstream.com 2017
 import os
 import argparse
 import subprocess
-#from modules import the_extractorator
-#from modules import the_differentiator
+from modules import the_extractorator
+from modules import the_differentiator
 
-global mop_path
-mop_path = ''
 
-#def arguments():
-#    """ Parse entered CLI arguments with argparse """
-#    parser = argparse.ArgumentParser(description='Parse and compare before/after Pinky_Baseliner files.')
-#    parser.add_argument('-m', '--mop', help='Specify a MOP number to parse', required=True)
-#    parser.add_argument('-a', '--after', help='Keyword to identify "After" files (defautl=before', required=False)
-#    parser.add_argument('-b', '--before', help='Keyword to identify "Before" files (defautl=before', required=False)
-#    args = vars(parser.parse_args())
-#    if args['mop']:
-#        mop = args['mop']
-#    if args['before']:
-#        tag1 = args['before']
-#    else:
-#        tag1 = 'before'
-#    if args['after']:
-#        tag2 = args['after']
-#    else:
-#        tag2 = 'after'
-#    if mop == '':
-#        print("ERROR: Please enter a valid MOP number.  Use --help for usage info.")
-#        exit(1)
-#    return mop, tag1, tag2
+def arguments():
+    """ Parse entered CLI arguments with argparse """
+    parser = argparse.ArgumentParser(description='Parse and compare before/after Pinky_Baseliner files.')
+    parser.add_argument('-m', '--mop', help='Specify a MOP number to parse', required=True)
+    parser.add_argument('-a', '--after', help='Keyword to identify "After" files (defautl=before', required=False)
+    parser.add_argument('-b', '--before', help='Keyword to identify "Before" files (defautl=before', required=False)
+    args = vars(parser.parse_args())
+    if args['mop']:
+        mop = args['mop']
+    if args['before']:
+        tag1 = args['before']
+    else:
+        tag1 = 'before'
+    if args['after']:
+        tag2 = args['after']
+    else:
+        tag2 = 'after'
+    if mop == '':
+        print("ERROR: Please enter a valid MOP number.  Use --help for usage info.")
+        exit(1)
+    return mop, tag1, tag2
 ## testing args
-mop_number = '234484'
-before_kw = 'before'
-after_kw = 'after'
+#mop_number = '234484'
+#before_kw = 'before'
+#after_kw = 'after'
 
 
-def folder_search(ext, dirname, names):
+def folder_search(mop_number):
     """ Function to recursivly seach for a folder name """
-    global mop_path
-    for name in names:
-        if name.lower().endswith(ext):
-            print 'Found ' + os.path.join(dirname, name)
-            mop_path = os.path.join(dirname, name)
-            return
-    #if mop_path == '':
-    #    print("ERROR: MOP number not found!")
-    #    exit(1)
+    mop_path = ''
+    os.chdir('/opt/ipeng/mops/')
+    for root, dirnames, filenames in os.walk(u'.'):
+        for directory in dirnames:
+            if str(directory) == str(mop_number):
+                mop_path = os.path.abspath(root + '/' + directory)
+                return mop_path
+    if mop_path == '':
+        print("ERROR: MOP number not found!")
+        exit(1)
 
 
 def file_search(keyword1, keyword2):
@@ -71,14 +70,14 @@ def file_search(keyword1, keyword2):
 
 
 # Step 1: Get the CLI arguments
-#mop_number, before_kw, after_kw = arguments()
+mop_number, before_kw, after_kw = arguments()
 
 # Step 2: Find the baseline folder
-os.chdir('/opt/ipeng/mops/')
-os.path.walk('.', folder_search, mop_number)
+mop_path = folder_search(mop_number)
 
 # Step 3: Get list of baseline files
 before_files, after_files = file_search(before_kw, after_kw)
+os.chdir('/opt/ipeng/scripts/baseline_parser/')
 
 # Step 4: Match the files up and parse:
 for i, item in enumerate(before_files):
@@ -90,17 +89,16 @@ for i, item in enumerate(before_files):
     device['files'].append(os.path.abspath(mop_path + "/" + after_files[i]))
     device['before_kw'] = before_kw
     device['after_kw'] = after_kw
-    #device['mop_path'] = mop_path
-    #device['mop_number'] = mop_number
     device['os_type'] = subprocess.Popen(
                     ["gimme", "os_type", device['hostname']], stdout=subprocess.PIPE).communicate()
     device['os_type'] = device['os_type'][0].rstrip()
-    print "Running " + device['hostname']
+    print "\nRunning " + device['hostname']
+    print "---------------------------"
 
     # Extract commands & output
     device['output'] = the_extractorator.run(device)
 
     # Compare command output
-    results = the_differentiator(commands)
+    results = the_differentiator.run(device)
 
 # Step 5: Do some shit with the results
--- modules/the_differentiator.py
+++ modules/the_differentiator.py
@@ -4,6 +4,8 @@ baseline_compare module to compare command output from Pinky_baseliner
 john.tishey@windstream.com 2017
 """
 
+import os
+from modules import jinja2
 from modules import yaml
 
 # Step 1: Loop through commands, using pre-determined logic where available
@@ -20,10 +22,10 @@ def run(device):
             test_values = yaml.load(_f.read())
         before_cmd = device['output']['before'][test_values[0]['command']]
         after_cmd = device['output']['after'][test_values[0]['command']]
-        execute_diff(test_values, before_cmd, after_cmd)
+        execute_diff(device, test_values, before_cmd, after_cmd)
 
 
-def execute_diff(test_values, before, after):
+def execute_diff(device, test_values, before, after):
     """ Gets before/after command and yaml test values to compare """
     # First, determine the test from test_values:
     for line in before:  
@@ -33,24 +35,39 @@ def execute_diff(test_values, before, after):
             # Skip lines that include a blacklisted word or are not to be iterated
             if word in line:
                 skip_line = True
-            if test_values[0]['iterate'] != 'all':
-                if test_values[0]['iterate'] not in line:
-                    skip_line = True
+        if test_values[0]['iterate'] != 'all':
+            if test_values[0]['iterate'] not in line:
+                skip_line = True
         if skip_line is not True:
             if 'no-diff' in test_values[0]['tests'][0]:
                 line = line.split()
                 line_id = test_values[0]['tests'][0]['no-diff'][0]
                 for i, after_line in enumerate(after):
+                    pos_match = after_line
                     after_line = after_line.split()
                     if line[line_id] == after_line[line_id]:
                         for index in test_values[0]['tests'][0]['no-diff']:
                             if line[index] != after_line[index]:
-                                print("FAIL: " + line[line_id] + " was " + line[index] +" and is now " + after_line[index])
                                 fail_flag = True
                         if fail_flag == False:
-                            after.pop(i)
+                            after.remove(pos_match)
+                            break
             if fail_flag == True:
-                print(test_values[0]['tests'][0]['err']) 
+                msg = jinja2.Template(str(test_values[0]['tests'][0]['err']))
+                print msg.render(device=device, pre=line, post=after_line) 
             else:
-                print(test_values[0]['tests'][0]['info'])
-                print line[0] + ' - ' + line[1] + ' - ' + line[3]
+                msg = jinja2.Template(str(test_values[0]['tests'][0]['info']))
+                print msg.render(device=device, pre=line, post=after_line)
+    if len(after) > 0:
+        for after_line in after:
+            skip_line = False
+            for word in test_values[0]['blacklist']:
+                if word in after_line:
+                    skip_line = True
+            if skip_line == False:
+                after_line = after_line.split()
+                line = ['null','null','null','null','null','null','null','null',]
+                msg = jinja2.Template(str(test_values[0]['tests'][0]['err']))
+                #print msg.render(device=device, pre=line, post=after_line)
+                print 'FAILED! ISIS adj to ' + after_line[1] + ' on ' + after_line[0] + ' is now ' + after_line[3]
+
