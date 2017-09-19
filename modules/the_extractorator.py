d3be19dd2d1f136590c927840c69182d27860280
John <johntishey@gmail.com>
2017-09-19 01:48:52 -0400

    update

--- modules/the_extractorator.py
+++ modules/the_extractorator.py
@@ -7,12 +7,21 @@ john.tishey@windstream.com 2017
 # Step 1: Get the device hostname and Vendor/OS
 def run(device):
     """ Get device and baselines """
+<<<<<<< 2c201c30a1ff68d97a6394a29b772572b1a30cb6
     if device['os_type'] == 'JUNOS':
         prompt = 'deip@' + device['hostname']
     elif device['os_type'] == 'IOS':
         prompt = device['hostname'] + '#'
     elif device['os_type'] == 'xr':
         prompt = 'RP/0/RP0/CPU0:' + device['hostname'] + '#'    
+=======
+    if device.os_type == 'JUNOS':
+        prompt = 'deip@' + device.hostname
+    elif device.os_type == 'IOS':
+        prompt = device.hostname + '#'
+    elif device.os_type == 'XR':
+        prompt = 'RP/0/RSP0/CPU0:' + device.hostname + '#'    
+>>>>>>> updates
     else:
         print("ERROR: Device OS not found or not yet supported")
         return ""
@@ -24,7 +33,7 @@ def extract(device, prompt):
     """ extract commands from baselines """
     # Open baseline files and loop through lines
     output = {}
-    for each_file in device['files']:
+    for each_file in device.files:
         with open(each_file) as _f:
             baseline_text = _f.readlines()
         commands = {}
@@ -44,7 +53,7 @@ def extract(device, prompt):
                 if current_command is not '':
                     if line != '' and line != '{master}':
                         commands[current_command].append(line)
-        if device['before_kw'] in each_file:
+        if device.config.before_kw in each_file:
             output['before'] = commands
         else:
             output['after'] =  commands
